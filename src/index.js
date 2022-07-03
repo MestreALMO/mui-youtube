d5ea63c55293285b53688c67dae08549dd022a0e
André Lusegardis <lusegardis@gmail.com>
2022-07-03 20:34:38 -0300

    realocated lines

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { CssBaseline } from "@mui/material";
 import { StylesGlobal } from "./styles/stylesGlobal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
